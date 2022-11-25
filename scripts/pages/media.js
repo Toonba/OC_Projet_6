7993d8b3b4c102444ade6a53f19759d8016bd82e
Aymerick Toonba <godzilla.toonba@gmail.com>
2022-11-25 12:32:11 +0100

    sorting function but atm don't work when clicking on filter div

--- scripts/pages/media.js
+++ scripts/pages/media.js
@@ -5,23 +5,75 @@ async function getPhotographersMedia() {
 
 async function displayGalery(media, filter) {
     let urlId = new URLSearchParams(window.location.search).get("id");
+    let mediaById = [];
     for (let i = 0; i < media.length; i++) {
         if (urlId == media[i].photographerId) {
-            photographerGalery = galeryFactory(media[i]);
-            photographerGalery.getPhotographeGaleryDOM();
+            mediaById.push(media[i]);
         }
     }
+    media = mediaById;
+    getMediaSorted(media, filter);
+    return mediaById;
 }
 
 async function init() {
     // Récupère les datas des photographes
     const {media} = await getPhotographersMedia();
-    displayGalery(media, popularity);
+    displayGalery(media, "popularity");
 }
-
+async function sort(filter) {
+    const {media} = await getPhotographersMedia();
+    displayGalery(media, filter);
+}
 init();
 
-//filter part 
+function getMediaSorted(array, filter) {
+    for (i = 0; i < array.length; i++) {
+        switch (filter) {
+            case "popularity":
+                array.sort(function compare(a, b) {
+                    if (a.likes < b.likes) {
+                        return 1;
+                    } else if (a.likes > b.likes) {
+                        return -1;
+                    } else {
+                        return 0;
+                    }
+                });
+                photographerGalery = galeryFactory(array[i]);
+                photographerGalery.getPhotographeGaleryDOM();
+                break;
+            case "date":
+                array.sort(function compare(a, b) {
+                    if (a.date < b.dat) {
+                        return -1;
+                    } else if (a.date > b.date) {
+                        return 1;
+                    } else {
+                        return 0;
+                    }
+                });
+                photographerGalery = galeryFactory(array[i]);
+                photographerGalery.getPhotographeGaleryDOM();
+                break;
+            case "title":
+                array.sort(function compare(a, b) {
+                    if (a.title < b.title) {
+                        return -1;
+                    } else if (a.title > b.title) {
+                        return 1;
+                    } else {
+                        return 0;
+                    }
+                });
+                photographerGalery = galeryFactory(array[i]);
+                photographerGalery.getPhotographeGaleryDOM();
+                break;
+        }
+    }
+}
+
+//filter part
 const filterOption = document.getElementsByClassName("dropdown-option");
 const filterBtn = document.getElementsByClassName("filter-button");
 const dropdown = document.getElementsByClassName("dropdown");
@@ -37,11 +89,4 @@ for (let i = 0; i < filterOption.length; i++) {
         dropdown[0].style.display = "none";
         btnContent[0].textContent = filterOption[i].textContent;
     });
-}
-
-function sortByPopularity(array) {
-    for (let i = 0; i< array.length; i++){
-        if (array[i].likes
-    }
-
 }
